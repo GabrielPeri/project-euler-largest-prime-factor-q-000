321e0ee1dab587aaeefb8f6fb3aae64d26825e9d
GabrielPeri <gabriel.w.peri@gmail.com>
2016-02-22 21:09:05 +0000

    nitrous-backup at Mon Feb 22 21:09:05 UTC 2016

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,18 @@
-# Enter your procedural solution here!
+def largest_prime_factor(input)
+  number = 2
+  prime_total = 0
+  prime_array = []
+  test = input
+
+  until prime_total >= input
+    if test % number == 0
+      prime_array << number
+      prime_total = prime_array.inject(:*)
+      test = test / number
+    elsif test % number != 0
+      number +=1
+    end
+  end
+
+  return prime_array[-1]
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,21 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+
+  def largest_prime_factor(input)
+    number = 2
+    prime_total = 0
+    prime_array = []
+    test = input
+
+    until prime_total >= input
+      if test % number == 0
+        prime_array << number
+        prime_total = prime_array.inject(:*)
+        test = test / number
+      elsif test % number != 0
+        number +=1
+      end
+    end
+
+    return prime_array[-1]
+  end
+end
